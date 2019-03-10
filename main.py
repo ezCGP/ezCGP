61a097ccd95c8f3c7f27064e4e611d2ab03d01d1
Rodd Talebi <roddtalebi@gmail.com>
2019-03-10 13:35:41 -0400

    fixed random seed issue...need to set seed before imports

--- main.py
+++ main.py
@@ -2,20 +2,22 @@
 
 # external packages
 from copy import deepcopy
+import numpy as np
 
-import problem
-
-# my scripts
-import universe
-num_universes = 20
 
 if __name__ == '__main__':
+    # set the seed and import scripts
+    seed = 4
+    np.random.seed(seed)
+    import problem
+    import universe
+
     # Read in Data
     train_data = problem.x_train
     train_labels = problem.y_train
-    seed = 10
 
     final_populations = [] # one for each universe created
+    num_universes = 1#20
     for i in range(num_universes):
         print("start new run %i" % i)
         converged_solution = universe.create_universe(input_data=train_data, labels=train_labels, universe_seed=seed+i)
