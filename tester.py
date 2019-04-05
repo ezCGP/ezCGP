77294c10f5f988db1ed1254093c8c7f672c83624
Aniruddha Das <aniruddhadas1998@gmail.com>
2019-04-04 23:32:26 -0400

    Updated tester so that we can see the values of the arguments being used.

--- tester.py
+++ tester.py
@@ -7,21 +7,32 @@ train_labels = problem.y_train
 
 individual = Individual(problem.skeleton_genome)
 individual.evaluate(problem.x_train, problem.y_train, (problem.x_val, problem.y_val))
+individual.fitness.values = problem.scoreFunction(actual=problem.y_val, predict=individual.genome_outputs)
+print('individual has fitness: ', individual.fitness.values)
+
 # print(individual.genome_outputs)
 # print(individual.dead)
 for i in range(1,individual.num_blocks+1):
     curr_block = individual.skeleton[i]["block_object"]
+    # print('curr_block: {}'.format(curr_block))
     arg_values = np.array(curr_block.args)
     print('arg_values: {}'.format(arg_values))
     print('curr_block isDead = ', curr_block.dead)
     print(curr_block.active_nodes)
     for active_node in curr_block.active_nodes:
         fn = curr_block[active_node]
-        # print(type(fn))
-        # print('fn[args]: {}'.format(fn['args']))
-        print('function at: {} is: {}'\
-            .format(active_node, fn))
-        # print('Args are: ')
+        if active_node < 0:
+            # nothing to evaluate at input nodes
+            print('function at: {} is: {}'\
+                .format(active_node, fn))
+            continue
+        elif active_node >= curr_block.genome_main_count:
+            # nothing to evaluate at output nodes
+            print('function at: {} is: {} -> likely an output node'\
+                .format(active_node, fn))
+            continue
+        print('function at: {} is: {} and has arguments: {}'\
+                .format(active_node, fn, arg_values[fn['args']]))
 
 individual.mutate()
 print('\n\nPOST MUTATE')
@@ -33,8 +44,18 @@ for i in range(1,individual.num_blocks+1):
     print(curr_block.active_nodes)
     for active_node in curr_block.active_nodes:
         fn = curr_block[active_node]
-        # print(fn)
-        print('function at: {} is: {}'.format(active_node, fn))
+        if active_node < 0:
+            # nothing to evaluate at input nodes
+            print('function at: {} is: {}'\
+                .format(active_node, fn))
+            continue
+        elif active_node >= curr_block.genome_main_count:
+            # nothing to evaluate at output nodes
+            print('function at: {} is: {} -> likely an output node'\
+                .format(active_node, fn))
+            continue
+        print('function at: {} is: {} and has arguments: {}'\
+                .format(active_node, fn, arg_values[fn['args']]))
 
 # print('labels: {} have shape: {}\ngenome_outputs: {} have shape: {}'\
 #     .format(train_labels, train_labels.shape, individual.genome_outputs, individual.genome_outputs.shape))
