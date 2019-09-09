ea060475781b81c58bbe68fc73bd73e15a929a30
Samuel Zhang <samuelzhang3@gmail.com>
2019-09-09 17:12:40 -0400

    removed testing comments for multi-push but single-pull public vs. private git management

--- problem.py
+++ problem.py
@@ -10,11 +10,6 @@ from utils.preprocessing_block import PreprocessingBlock
 from utils.DbConfig import DbConfig
 from utils.DbManager import DbManager
 
-# testing a small commit
-# testing a small commit for both branches part 2
-# testing a small commit for both branches part 3
-# testing pull from only public github
-
 generation_limit = 19
 score_min = 0.00 # terminate immediately when 100% accuracy is achieved
 
