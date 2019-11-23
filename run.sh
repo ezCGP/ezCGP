fda8301fbf72e1e8d57793fe1aebbc303c035488
Samuel Zhang <samuelzhang3@gmail.com>
2019-11-23 11:09:41 -0500

    updated macos run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@
 #mpiexec -n 1 ~/anaconda3/envs/ezCGP/bin/python mpi_universe.py
 #OMP_NUM_THREADS=8 KMP_WARNING=OFF mpiexec -n 20 ~/anaconda3/envs/ezCGP/bin/python mpi_universe.py
 #python main.py
-#OMP_NUM_THREADS=1 KMP_WARNING=OFF OMP_PROC_BIND=FALSE mpiexec.mpich -n 2 python3 mpi_universe.py
+
+# Sam's MacOS script
+OMP_NUM_THREADS=1 KMP_WARNING=OFF OMP_PROC_BIND=FALSE mpiexec -n 2 python3 mpi_universe.py
 
 # Mai's script
-OMP_NUM_THREADS=1 KMP_WARNING=OFF mpiexec.mpich -n 2 ~/anaconda3/envs/ezCGP/bin/python mpi_universe.py
+#OMP_NUM_THREADS=1 KMP_WARNING=OFF mpiexec.mpich -n 2 ~/anaconda3/envs/ezCGP/bin/python mpi_universe.py
