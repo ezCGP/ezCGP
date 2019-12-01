8a1bc568317903996f2bdb21a68ca6aff3244be8
Samuel Zhang <szhang460@pace-ice.pace.gatech.edu>
2019-12-01 05:09:04 -0500

    removed extraneous stuff from pace script

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #python main.py
 
 # Sam's MacOS script
-OMP_NUM_THREADS=1 KMP_WARNING=OFF OMP_PROC_BIND=FALSE mpiexec -n 2 python3 mpi_universe.py
+OMP_NUM_THREADS=1 KMP_WARNING=OFF OMP_PROC_BIND=FALSE mpiexec -n 12 python3 mpi_universe.py
 
 # Mai's script
 #OMP_NUM_THREADS=1 KMP_WARNING=OFF mpiexec.mpich -n 2 ~/anaconda3/envs/ezCGP/bin/python mpi_universe.py
