8b81d6f8f014d137f67083562708ccd35b9e6867
Samuel Zhang <samuelzhang3@gmail.com>
2019-12-03 10:23:02 -0500

    added clearer print statements for convert

--- convert.py
+++ convert.py
@@ -8,11 +8,13 @@ import problem
 # THIS IS NOT POLISHED CODE!!!!!!
 def convert(individuals):
     s = ''
-    for ind_1 in individuals:
+    for i, ind_1 in enumerate(individuals):
+        print('Individual number {}:'.format(i))
         ind_1 = individual.build_individual(problem.skeleton_genome, ind_1)
         print("fitness", ind_1.fitness.values)
         for i in range(1,ind_1.num_blocks+1):
             curr_block = ind_1.skeleton[i]["block_object"]
+            print('{} Block:'.format(type(curr_block)))
             for active_node in curr_block.active_nodes:
                 print(curr_block[active_node])
                 s += str(curr_block[active_node])
