55bb820e77223b22d65a4f231b32b0cc08a2aaa9
Samuel Zhang <samuelzhang3@gmail.com>
2019-12-03 11:59:24 -0500

    cleaned up convert to be pretty and readable with its print statements

--- convert.py
+++ convert.py
@@ -1,33 +1,44 @@
+import sys
 import numpy as np
 import individual
-import problem
+from problem import skeleton_genome
 # convert from npy to txt file and shows blocks and their layers
-# YOU MUST CHANGE THE TXT FILE STRING AND SPECIFY WHICH GEN TO WRITE IN OUTPUTS
-# OTHERWISE IT WILL OVERWRITE!!!!!
 #
-# THIS IS NOT POLISHED CODE!!!!!!
+# USAGE
+#
+# python3 convert.py <file_name>
+#
+# where <file_name> is the name of the .npy file
+#
+# e.g. python3 convert.py gen4_pop.npy
+
 def convert(individuals):
     s = ''
     for i, ind_1 in enumerate(individuals):
-        print('\nIndividual number {}:'.format(i))
-        ind_1 = individual.build_individual(problem.skeleton_genome, ind_1)
-        print("fitness", ind_1.fitness.values)
+        s += '\n\nIndividual number {}:'.format(i)
+        ind_1 = individual.build_individual(skeleton_genome, ind_1)
+        s += "\nfitness {}".format(ind_1.fitness.values)
         for i in range(1,ind_1.num_blocks+1):
             # get block dictionary containing metadata + block obj
             curr_block = ind_1.skeleton[i]
 
             # show block name
-            print('\n{} Block:'.format(curr_block['nickname']))
+            s += '\n\n{} Block:'.format(curr_block['nickname'])
 
             # go through each active genome node and print
             for active_node in curr_block['block_object'].active_nodes[:-1]:
                 # print all layers except last node because it is just a number
                 # i'm not sure why, but it isn't a layer type so it should probably be ignored
-                print(curr_block['block_object'][active_node])
-                s += str(curr_block['block_object'][active_node])
+                s += '\n' + str(curr_block['block_object'][active_node])
+
+    print(s) # print total file output
     return s
 
-data = np.load('outputs_cifar/gen4_pop.npy', allow_pickle=True)
+try:
+    data = np.load('outputs_cifar/{}'.format(sys.argv[1]), allow_pickle=True)
+except IndexError:
+    print('Please input a gen .npy file from outputs_cifar')
+    quit()
 s = convert(data)
 #text_file = open("gen6_pop.txt", "w")
 #text_file.write(s)
