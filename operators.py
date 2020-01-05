58a56257e1baa0c4a42731f6ba9ffca47dba899f
Samuel Zhang <samuelzhang3@gmail.com>
2020-01-04 19:14:48 -0500

    added batch normalization function back to training block

--- operators.py
+++ operators.py
@@ -339,11 +339,11 @@ def batch_normalization_func(input_tensor):
     # Batch Normalization
     return tf.layers.batch_normalization(input_tensor, training=True)
 
-operDict[concat_func] = {"inputs": [tf.Tensor],
-                         "args": [],
-                         "outputs": tf.Tensor,
-                         "name": "batch_normalization",
-                         "include_labels": False}
+operDict[batch_normalization_func] = {"inputs": [tf.Tensor],
+                                      "args": [],
+                                      "outputs": tf.Tensor,
+                                      "name": "batch_normalization",
+                                      "include_labels": False}
 
 
 def relu_func(input_tensor):
