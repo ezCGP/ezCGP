766f025115269bd577f6d863879af97ff47de6ca
Samuel Zhang <samuelzhang3@gmail.com>
2020-01-18 18:31:08 -0500

    updated conda install script with absolute location in GCP

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ cd ~
 git clone https://github.com/ezCGP/ezCGP
 cd ezCGP
 git checkout 2020S-gpu
-conda create -n ezCGP python=3.6 anaconda -y
-conda activate ezCGPP
-conda config --env --add channels menpo
-conda config --env --add channels conda-forge
-conda install --file requirements.txt -y
-pip install horovod
+sudo /root/anaconda3/bin/conda create -n ezCGP python=3.6 anaconda -y
+sudo /root/anaconda3/bin/conda activate ezCGPP
+sudo /root/anaconda3/bin/conda config --env --add channels menpo
+sudo /root/anaconda3/bin/conda config --env --add channels conda-forge
+sudo /root/anaconda3/bin/conda install --file requirements.txt -y
+sudo pip install horovod
