9b39d577ec3ac90da59a851061357fe0bd659f8a
Samuel Zhang <samuelzhang3@gmail.com>
2020-01-18 18:14:14 -0500

    added scripts to automate GCP basic install and for install.sh for user to run on login

---
+++ install.sh
@@ -0,0 +1,17 @@
+# install shared Anaconda environment following: https://medium.com/@pjptech/installing-anaconda-for-multiple-users-650b2a6666c6
+cd /tmp
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+bash Anaconda3*.sh -u
+chmod -R go-w /opt/anaconda
+chmod -R go+rX /opt/anaconda
+source ~/.bashrc
+cd ~
+git clone https://github.com/ezCGP/ezCGP
+cd ezCGP
+git checkout 2020S-gpu
+conda create -n ezCGP python=3.6 anaconda -y
+conda activate ezCGPP
+conda config --env --add channels menpo
+conda config --env --add channels conda-forge
+conda install --file requirements.txt -y
+pip install horovod
