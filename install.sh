2d1083af3a0ed4052c8d71faa9bb0667d113dfd3
Samuel Zhang <samuelzhang3@gmail.com>
2020-01-18 19:33:14 -0500

    added pip3 to install

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ cd ~
 git clone https://github.com/ezCGP/ezCGP
 cd ezCGP
 git checkout 2020S-gpu
-/root/anaconda3/bin/conda create -n ezCGP python=3.6 anaconda -y
-/root/anaconda3/bin/conda activate ezCGPP
-/root/anaconda3/bin/conda config --env --add channels menpo
-/root/anaconda3/bin/conda config --env --add channels conda-forge
-/root/anaconda3/bin/conda install --file requirements.txt -y
+conda create -n ezCGP python=3.6 anaconda -y
+conda activate ezCGPP
+conda config --env --add channels menpo
+conda config --env --add channels conda-forge
+conda install --file requirements.txt -y
 pip3 install horovod
