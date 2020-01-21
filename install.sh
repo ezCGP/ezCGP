ee5acfbffebe932fc22a905e65e0b84611620d43
Samuel Zhang <samuelzhang3@gmail.com>
2020-01-21 12:36:13 -0500

    updated shell config after conda install

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ cd ~
 git clone https://github.com/ezCGP/ezCGP
 cd ezCGP
 git checkout 2020S-gpu
+sudo ln -s $HOME/anaconda3/etc/profile.d/conda.sh /etc/profile.d/conda.sh
 conda create -n ezCGP python=3.6 anaconda -y
 conda activate ezCGPP
 conda config --env --add channels menpo
 conda config --env --add channels conda-forge
 conda install --file requirements.txt -y
+source ~/.bashrc
 pip3 install horovod
