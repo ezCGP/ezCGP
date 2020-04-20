25e97603a1205cce120250affdfbb68220c25208
Kinnera Banda <kbanda3@pace-ice.pace.gatech.edu>
2020-04-19 21:01:56 -0400

    addind changed problem.py file

--- kinnera_ezCGP_runs/run_1/problem.py
+++ kinnera_ezCGP_runs/run_1/problem.py
@@ -18,7 +18,7 @@ import logging
 # divide by num cores to find number of individuals per core
 # Create Universe: cpu cores == pop_size
 
-GEN_LIMIT = 100
+GEN_LIMIT = 50
 POP_SIZE = 20
 N_EPOCHS = 10
 SEED = 17
@@ -30,7 +30,8 @@ N_OFFSPRING = 2 # THIS COMES IN PAIRS (e.g. N_OFFPSRING = 2 is 4/gen)
 
 MIN_SCORE = 0.00  # terminate immediately when 100% accuracy is achieved
 
-SEED_ROOT_DIR = 'sam_ezCGP_runs/run_20'
+SEED_ROOT_DIR = 'kinnera_ezCGP_runs/run_1'
+#SEED_ROOT_DIR = 'sam_ezCGP_runs/run_20'
 #SEED_ROOT_DIR = 'jinghua_ezCGP_runs'
 
 """DBMANAGER"""
@@ -134,18 +135,18 @@ def scoreFunction(predict, actual):
         .format(x_train.shape, y_train.shape, x_test.shape, y_test.shape))
 
 """
-preprocessing_block1 = PreprocessingBlock(nickname='Data Augmentation', tensorblock_flag=False, apply_to_val=False, main_count=50, n_epochs=N_EPOCHS)
-preprocessing_block2 = PreprocessingBlock(nickname='Preprocessing', tensorblock_flag=False, apply_to_val = True, main_count=1, n_epochs=N_EPOCHS,
-                                           primitives={operators.ceil_greyscale_norm: {'prob': 1}}) #input_dtypes = [tf.Tensor], output_dtypes = [tf.Tensor])
+#preprocessing_block1 = PreprocessingBlock(nickname='Data Augmentation', tensorblock_flag=False, apply_to_val=False, main_count=50, n_epochs=N_EPOCHS)
+#preprocessing_block2 = PreprocessingBlock(nickname='Preprocessing', tensorblock_flag=False, apply_to_val = True, main_count=1, n_epochs=N_EPOCHS,
+                                           #primitives={operators.ceil_greyscale_norm: {'prob': 1}}) #input_dtypes = [tf.Tensor], output_dtypes = [tf.Tensor])
 
-training_block = TrainingBlock(nickname='Training', main_count=120, learning_required=True, apply_to_val=False, n_epochs=N_EPOCHS)
+training_block = TrainingBlock(nickname='Training', main_count=70, learning_required=True, apply_to_val=False, n_epochs=N_EPOCHS)
 
 # Defines the generic genome structure
 skeleton_genome = {
     'input': [np.ndarray],  # we don't pass in the labels since the labels are only used at evaluation and scoring time
     'output': [np.ndarray],
     # vars converts the block object to a dictionary
-    1: vars(preprocessing_block1),
-    2: vars(preprocessing_block2),
-    3: vars(training_block)
+    #1: vars(preprocessing_block1),
+    #2: vars(preprocessing_block2),
+    1: vars(training_block)
 }
