be21f71181cf641dd345243f6665fc3c7d787241
HemangRajvanshy <hemangrajvanshy@gmail.com>
2020-09-14 22:24:27 -0400

    Ported conv2D and added conv2DTranpose

--- codes/block_definitions/utilities/operators_TensorFlow_Keras.py
+++ codes/block_definitions/utilities/operators_TensorFlow_Keras.py
@@ -11,31 +11,63 @@ And make sure argument types are valid and appropriate for your needs
 '''
 
 
-### packages
+# packages
+from codes.utilities.custom_logging import ezLogging
+from codes.block_definitions.utilities import argument_types
 import tensorflow as tf
 import numpy as np
 
-### sys relative to root dir
+# sys relative to root dir
 import sys
 from os.path import dirname, realpath
 sys.path.append(dirname(dirname(dirname(dirname(realpath(__file__))))))
 
-### abosulte imports wrt root
-from codes.block_definitions.utilities import argument_types
-from codes.utilities.custom_logging import ezLogging
+# abosulte imports wrt root
 
 
-
-### init dict
+# init dict
 operator_dict = {}
 
 
-def ting(input, arg):
-    pass
+def conv2D_layer(input_tensor, filters=64, kernel_size=3, activation=tf.nn.relu):
+    kernel_size = (kernel_size, kernel_size)
+    '''
+    Convolutional Layer
+    Computes 32 features using a 5x5 filter with ReLU activation.
+    Padding is added to preserve width and height.
 
-operator_dict[ting] = {"inputs": [datatype0],
-                       "output": datatype0,
-                       "args": argument_types.argtype0
-                      }
+    https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv2D
+    '''
+
+    return tf.keras.layers.Conv2D(filters=filters,
+                                  kernel_size=kernel_size,
+                                  padding="same",
+                                  activation=activation,
+                                  data_format="channels_last"
+                                  )(input_tensor)
 
 
+operator_dict[conv2D_layer] = {"inputs": [tf.keras.layers],
+                               "output": tf.keras.layers,
+                               "args": [argument_types.ArgumentType_Pow2, argument_types.ArgumentType_TFFilterSize, argument_types.ArgumentType_TFActivation]
+                               }
+
+
+def conv2DTranspose_layer(input_tensor, filters=64, kernel_size=3, activation=tf.nn.relu):
+    kernel_size = (kernel_size, kernel_size)
+    '''
+    https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv2DTranspose
+    '''
+
+    return tf.keras.layers.Conv2DTranspose(filters=filters,
+                                           kernel_size=kernel_size,
+                                           padding="same",
+                                           activation=activation,
+                                           data_format="channels_last"
+                                           )(input_tensor)
+
+
+operator_dict[conv2DTranspose_layer] = {"inputs": [tf.keras.layers],
+                                        "output": tf.keras.layers,
+                                        "args": [argument_types.ArgumentType_Pow2, argument_types.ArgumentType_TFFilterSize, argument_types.ArgumentType_TFActivation]
+                                        }
