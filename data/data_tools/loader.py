d8667dea9c61d75aa159c5641fba9aeee60249c2
Hoa Luu <hluu8@gatech.edu>
2020-10-19 00:33:53 -0400

    updata loader.py
    update image shape information

--- data/data_tools/loader.py
+++ data/data_tools/loader.py
@@ -163,6 +163,7 @@ class ezDataLoader_MNIST(ezDataLoader):
 
     def load(self):
         # https://www.tensorflow.org/api_docs/python/tf/keras/datasets/mnist/load_data
+        # image shape is (Samples, width, height, channels)
         import tensorflow as tf
         mnist = tf.keras.datasets.mnist
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
