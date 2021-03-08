38c82cf1fcf7f26b5434bef6860dcd6eea7cd145
Rodd <roddtalebi@gmail.com>
2021-03-08 16:42:32 -0500

    small local edits for pace

--- misc/plot_pareto_front.py
+++ misc/plot_pareto_front.py
@@ -27,17 +27,17 @@ if __name__ == "__main__":
                         nargs='+', # 1 or more
                         help='List directories for each universe run',
                         required=True)
-
+    args = parser.parse_args()
 
     fig, axis = plt.subplots(1, 1, figsize=(10,15))
 
     generation = 0
-    for dir_path in parser.dirs:
+    for dir_path in args.dirs:
         npzs = glob.glob(os.path.join(dir_path, "gen*_fitness.npz"))
         for npz in npzs:
             plot_things.plot_pareto_front_from_fitness_npz(axis,
                                                            npz,
-                                                           minimization=parser.minimization,
+                                                           minimization=args.minimization,
                                                            color=plot_things.matplotlib_colors[generation],
                                                            label="Gen %2d" % generation,
                                                           )
