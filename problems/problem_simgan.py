3fa7090fd6eeea3fab9d1afc9c823bd4abd6688d
Rodd <roddtalebi@gmail.com>
2022-01-26 17:55:59 -0500

    didn't like how it was connecting ith_indiv to it's refiner...fixed some indexing and code to my liking...still not ideal imo because it assumes that there is a 1-1 matching which, for now, is a good assumption but those things always make me uneasy

--- problems/problem_simgan.py
+++ problems/problem_simgan.py
@@ -107,14 +107,14 @@ class Problem(ProblemDefinition_Abstract):
         n_individuals = len(population.population)
         refiners = []
         discriminators = []
-        indiv_inds = []
-        bad_indiv_inds = []
+        #indiv_inds = []
+        #bad_indiv_inds = []
         # import pdb; pdb.set_trace()
         for i, indiv in enumerate(population.population):
             if indiv.dead:
                 indiv.fitness.values = (np.inf,)
             else:
-                indiv_inds.append(i)
+                #indiv_inds.append(i)
                 R, D = indiv.output
                 refiners.append(R.cpu())
                 discriminators.append(D.cpu())
@@ -126,9 +126,11 @@ class Problem(ProblemDefinition_Abstract):
                                                    self.validating_datalist[0],
                                                    'cpu')
         
-        for refiner_rating, ind in zip(refiner_ratings['r'].to_numpy(), indiv_inds):
+        # instead of using indiv_inds I think we can just use the row label of the df
+        #for refiner_rating, ind in zip(refiner_ratings['r'].to_numpy(), indiv_inds):
+        for indx, refiner_rating in refiner_ratings['r'].iteritems():
             # Use negative ratings because ezCGP does minimization
-            population.population[ind].fitness.values = (-1 * refiner_rating,)
+            population.population[indx].fitness.values = (-1 * refiner_rating,)
 
 
     def check_convergence(self, universe):
